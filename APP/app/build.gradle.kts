2ef9b21c1d66bb7eb69e1f87b6217457a2d0b5ee
jayazzuro <vogiahao2005@gmail.com>
2025-05-31 11:00:08 +0700

    Sửa lần 2

--- APP/app/build.gradle.kts
+++ APP/app/build.gradle.kts
@@ -32,12 +32,16 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.volley)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
 }
